10c23c5ed88734074cb300910d385c3a9b9f8ca0
John Manack <jmnck28@gmail.com>
2021-03-05 14:39:55 -0500

    adds target blank to links

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,7 +1,7 @@
 const Details = (props) => {
     const { description, height, width, id, g , b } = props;
     return (
-        <p>{description} <a href={`https://picsum.photos/id/${id}/${width}/${height}/?${g}&${b}`}> https://picsum.photos/id/{id}/{width}/{height}/?{g}&{b}</a></p>
+        <p>{description} <a href={`https://picsum.photos/id/${id}/${width}/${height}/?${g}&${b}`} target="_blank" rel="noreferrer"> https://picsum.photos/id/{id}/{width}/{height}/?{g}&{b}</a></p>
     )
 }
 
